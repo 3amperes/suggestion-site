d2a30cc02e1dea644a8ff2790abd7e9f96208762
wabdsgn <wab2com@gmail.com>
2020-07-31 11:47:25 +0200

    🔍 add sitemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([optimizedImages]);
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [optimizedImages, { handleImages: ['jpeg', 'png'] }],
+  nextConfig
+);
